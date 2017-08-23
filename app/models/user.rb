a91c7bb15e92ac7d9282e245fd59b70a875f3d3f
Samantha Gongora <samantha.gongora@gmail.com>
2017-08-23 15:04:57 -0600

    adds test and functionality to create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  def self.update_or_create(auth)
+    user = User.find_by(uid: auth[:uid]) || User.new
+    user.attributes = {
+      provider: auth[:provider],
+      uid: auth[:uid],
+      name: auth[:info][:name],
+    }
+    user.save!
+    user
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "creates or updates itself from an oauth hash" do
+    auth = {
+      provider: "github",
+      uid: "12345678910",
+      info: {
+        name: "Samantha Gongora",
+      },
+    }
+    User.update_or_create(auth)
+    new_user = User.first
+
+    expect(new_user.provider).to eq("github")
+    expect(new_user.uid).to eq("12345678910")
+    expect(new_user.name).to eq("Samantha Gongora")
+  end
 end
